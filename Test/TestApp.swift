8672ce81411fdf842bcec5090fba31c18214d178
unsong3 <leeyc@korail.com>
2023-01-17 21:52:48 -0800

    Initial Commit

---
+++ Test/TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestApp.swift
+//  Test
+//
+//  Created by unsong3 on 2023/01/17.
+//
+
+import SwiftUI
+
+@main
+struct TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
